f4080d87207c3e1976e0a8b451fe598acfe8e488
Harun <harunrich@googlemail.com>
2017-12-31 23:28:05 +0000

    added message-view/updated app css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
+
 import './App.css';
+import messageView from './messages/message-view';
 
 class App extends Component {
 
     render(){
         return (
-            <div>
-                <h2>Hello World Again!</h2>
-                </div>
+            <messageView />
         )
     }
 }
