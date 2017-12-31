7d90d3d48169db4cf3b621efb547d9ab87a6fcf8
Harun <harunrich@googlemail.com>
2017-12-31 21:39:05 +0000

    moved JSX into variable

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-    <h1>Hello World</h1>,
-    document.getElementById('root')
-);
+const element = <div>Hello World</div>
+
+ReactDOM.render(element, document.getElementById('root'));
