2f0f40584f2a3d9b48c5526b9e3fce3209ac5c1f
Harun <harunrich@googlemail.com>
2018-01-01 13:55:37 +0000

    added a new post

--- src/messages/message-view.js
+++ src/messages/message-view.js
@@ -4,17 +4,34 @@ class MessageView extends Component {
     render() {
         return (
             <div className="container">
-                <div className="from">
-                    <span className="label">From: </span>
-                    <span className="value">Harun Richards</span>
+                <div className="msg">
+                    <div className="from">
+                        <span className="label">From: </span>
+                        <span className="value">Harun Richards</span>
+                    </div>
+                    <div className="status">
+                        <span className="label">Status: </span>
+                        <span className="value"> Unread</span>
+                    </div>
+                    <div className="message">
+                        <span className="label">Message: </span>
+                        <span className="value">Have a great day!</span>
+                    </div>
                 </div>
-                <div className="status">
-                    <span className="label">Status: </span>
-                    <span className="value"> Unread</span>
-                </div>
-                <div className="message">
-                    <span className="label">Message: </span>
-                    <span className="value">Have a great day!</span>
+                <hr/>
+                <div className="msg">
+                    <div className="from">
+                        <span className="label">From: </span>
+                        <span className="value">Amira Richards</span>
+                    </div>
+                    <div className="status">
+                        <span className="label">Status: </span>
+                        <span className="value"> Unread</span>
+                    </div>
+                    <div className="message">
+                        <span className="label">Message: </span>
+                        <span className="value">Have a great day!</span>
+                    </div>
                 </div>
             </div>
         );
