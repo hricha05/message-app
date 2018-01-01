671ca98c4ac981ae9edefd52b3314dcee1a1d512
Harun <harunrich@googlemail.com>
2018-01-01 15:46:06 +0000

    added MessageView proptype

--- src/messages/message-view.js
+++ src/messages/message-view.js
@@ -1,21 +1,25 @@
 import React, { Component} from 'react';
+import PropTypes from 'prop-types';
 
 class MessageView extends Component {
     render() {
+
+        const message = this.props.message;
+
         return (
             <div className="container">
                 <div className="msg">
                     <div className="from">
                         <span className="label">From: </span>
-                        <span className="value">Harun Richards</span>
+                        <span className="value">{message.from}</span>
                     </div>
                     <div className="status">
                         <span className="label">Status: </span>
-                        <span className="value"> Unread</span>
+                        <span className="value">{message.status}</span>
                     </div>
                     <div className="message">
                         <span className="label">Message: </span>
-                        <span className="value">Have a great day!</span>
+                        <span className="value">{message.content}</span>
                     </div>
                 </div>
                 <hr/>
@@ -34,7 +38,12 @@ class MessageView extends Component {
                     </div>
                 </div>
             </div>
-        );
+        )
+    }
+
+    //Mark message input parameter as required
+    MessageView.propTypes = {
+        message: PropTypes.object.isRequired
     }
 }
 
